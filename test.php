82ce45ab5a68342d33c215f894b8a54f78691237
Vector180ww <tihontihon008@gmail.com>
2025-02-21 09:43:26 +0200

    Update Vercel PHP runtime to 0.7.0

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?php
-$db = new SQLite3(':memory:');
-echo "SQLite3 работает!";
+$db = pg_connect('postgresql://neondb_owner:npg_uNx61pWBvDLC@ep-orange-waterfall-a89m7agu-pooler.eastus2.azure.neon.tech/neondb?sslmode=require');
+if (!$db) {
+    die("Ошибка подключения: " . pg_last_error());
+}
+echo "Подключение успешно!";
 ?>
